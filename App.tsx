3aee1af61ce1ead7fc25e3846d2ba5faca7321eb
awnishnegi <awnish.negi@okoders.com>
2023-07-10 14:40:50 +0530

    push working

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
@@ -9,7 +9,7 @@ import Counter from './src/screens/Counter';
 import NewCounter from './src/screens/NewCounter';
 import { setPageList } from './src/redux/action/pageList';
 import UserHome from './src/screens/UsersHome';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, Platform } from 'react-native';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 import ProductDetail from './src/screens/ProductDetail';
 import getQueryParameters from './src/utilities/getQueryParameters';
@@ -25,7 +25,8 @@ function App(): JSX.Element {
   useEffect(() => {
     fcmtoken()
   }, [])
-
+  // const navigation = useNavigation();
+  const [initialRoute, setInitialRoute] = useState('Home');
   const fcmtoken = async () => {
     console.log(
       await messaging().getToken())
@@ -98,28 +99,91 @@ function App(): JSX.Element {
 
   }
   useEffect(() => {
-    requestUserPermission()
-  }, [])
-  
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      console.log('A new FCM message arrived!', remoteMessage);
-    });
+    try {
+      requestUserPermission()
+    } catch (err) {
+      console.log("push err", err);
+    }
 
-    return unsubscribe;
+  }, [])
+
+  useEffect(() => {
+    try {
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        console.log('A new FCM message arrived!', remoteMessage);
+      });
+
+      return unsubscribe;
+    } catch (err) {
+      console.log("push err", err);
+
+    }
+
   }, []);
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
-  });
+  try {
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+      // navigate("Counter")
+    });
+  } catch (err) {
+    console.log("push err", err);
+
+  }
+  // const NotificationPermission = () => {
+    const checkApplicationPermission = async () => {
+      if (Platform.OS === 'android') {
+        try {
+          await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+          );
+        } catch (error) {
+        }
+      }
+    };
+    useEffect(() => {
+      checkApplicationPermission()
+    }, [])
+    useEffect(() => {
+      // Assume a message-notification contains a "type" property in the data payload of the screen to open
+  
+      messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage,
+        );
+        //@ts-ignore
+        // navigation.navigate("Counter");
+        setInitialRoute("Home");
+      });
+  
+      // Check whether an initial notification is available
+      messaging()
+        .getInitialNotification()
+        .then(remoteMessage => {
+          if (remoteMessage) {
+            console.log(
+              'Notification caused app to open from quit state:',
+              remoteMessage,
+            );
+            setInitialRoute("Home");
+            //@ts-ignore
+            // navigation.navigate("Counter");
+            // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+          }
+          // setLoading(false);
+        });
+    }, []);
   return (
     <NavigationContainer >
       <HandleDeepLinking />
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName={initialRoute}>
         {/* <Stack.Screen name="PushNotification" component={PushNotification} /> */}
+        
+        <Stack.Screen name="Counter" component={NewCounter} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ProductDetail" component={ProductDetail} />
 
-        {/* <Stack.Screen name="Counter" component={NewCounter} /> */}
+        
         {/* <Stack.Screen name="Counter" component={UserHome} /> */}
       </Stack.Navigator>
     </NavigationContainer>
